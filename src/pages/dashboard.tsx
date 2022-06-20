ce86cc9445c79c85063061e9ffedbe72f58be4a1
Kayky Casagrande <kayky-cas@github.com>
2022-06-20 00:06:27 -0300

    tailwind + votepage preview

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import { prisma } from '../db/client';
+
+const Dashboard: NextPage<any> = ({ counts }) => {
+  return (
+    <div>
+      <h1>We have {counts} pokemons cached on the database!</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
+
+export const getServerSideProps = async () => {
+  const pokemons = await prisma.pokemon.findMany();
+
+  return { props: { counts: pokemons.length } };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,30 @@
+import { Pokemon } from '@prisma/client';
 import type { NextPage } from 'next';
+import PokemonView, { PokemonNecessary } from '../components/pokemon';
 import { prisma } from '../db/client';
 
 const PokeVote: NextPage<any> = ({ counts }) => {
+  const pokemon1: PokemonNecessary = {
+    id: 1,
+    name: 'Bulbasaur',
+    imageUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+  };
+
+  const pokemon2: PokemonNecessary = {
+    id: 2,
+    name: 'Ivysaur',
+    imageUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
+  };
+
   return (
-    <div>
-      <h1>We have {counts} pokemons cached on the database!</h1>
+    <div className="h-screen w-screen bg-neutral-800">
+      <div className="md:container md:mx-auto h-screen flex w-screen">
+        <PokemonView />
+      </div>
     </div>
   );
 };
 
 export default PokeVote;
-
-export const getServerSideProps = async () => {
-  const pokemons = await prisma.pokemon.findMany();
-
-  return { props: { counts: pokemons.length } };
-};
